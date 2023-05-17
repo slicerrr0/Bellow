aa8bc34f1c8bf17a1be578352522a582bd62ecf5
slicerrr0 <mrslicerr0@protonmail.com>
2023-05-17 16:17:04 -0400

    add rest_framework and escrow to installed apps

--- Bellow/settings.py
+++ Bellow/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_celery_results',
+    'rest_framework',
     'home',
     'account',
+    'escrow',
 ]
 
 MIDDLEWARE = [
--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 
 # Create your views here.
 
