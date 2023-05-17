b3dfd14ea68e717bcbd7abbc48be9fe1b049bf5e
slicerrr0 <mrslicerr0@protonmail.com>
2023-05-17 16:20:37 -0400

    create and register 'api' app

--- Bellow/settings.py
+++ Bellow/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'home',
     'account',
     'escrow',
+    'api',
 ]
 
 MIDDLEWARE = [
---
+++ api/serializers.py
@@ -0,0 +1,3 @@
+'''Serializer classes for use with the `Django Rest Framework` package.'''
+
+from rest_framework import serializers
