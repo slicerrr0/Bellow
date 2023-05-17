0aea015fed04030eaec5e3b6d65b432a93c13426
slicerrr0 <mrslicerr0@protonmail.com>
2023-05-17 18:25:56 -0400

    created custom validator for validatign username/email field and finalized sign in form

--- account/forms.py
+++ account/forms.py
@@ -1,17 +1,33 @@
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
+from .validators import UsernameOrEmailValidator
 
 class SignInForm(AuthenticationForm):
+    username = forms.CharField(
+        min_length=6,
+        max_length=64,
+        label=_('Username or email'),
+        widget=forms.TextInput(attrs={'autofocus': True}),
+        validators=[UsernameOrEmailValidator],
+        strip=True,
+        initial='username or email',
+        required=True
+    )
+    password = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password"}),
+        initial='password',
+        required=True
+    )
     class Meta:
         model = get_user_model()
-        fields = [
-
-        ]
+        fields = ('username', 'password')
 
 class RegistrationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = [
-            
-        ]
+        fields = ()
---
+++ account/validators.py
@@ -0,0 +1,30 @@
+'''Custom form field validators.'''
+
+import re
+from typing import Collection
+from django.core.exceptions import ValidationError
+from django.core.validators import _ErrorMessage, EmailValidator
+from django.utils.deconstruct import deconstructible
+from django.utils.translation import gettext_lazy as _
+
+@deconstructible
+class UsernameOrEmailValidator(EmailValidator):
+    '''Checks if the value is a valid username or email.'''
+    username_regex = '[a-zA-Z0-9_]+'
+    def __init__(
+            self,
+            message: _ErrorMessage | None = ...,
+            code: str | None = ..., 
+            whitelist: Collection[str] | None = ...
+        ) -> None:
+        super().__init__(message, code, whitelist)
+    def __call__(self, value: str | None) -> None:
+        # If value is not formatted as an email, verify it as a username first
+        if not '@' in value and re.search(self.username_regex, value) is not None:
+            return None
+        elif not '@' in value:
+            raise ValidationError(
+                _('%(value)s contains illegal username characters.'),
+                params={'value': value}
+            )
+        return super().__call__(value)
