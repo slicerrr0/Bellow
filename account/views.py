96c05c82018832b0a792ef36ed1f033c44cb7b30
slicerrr0 <mrslicerr0@protonmail.com>
2023-05-17 17:11:46 -0400

    created `forms.py` module for housing account-related forms

--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_http_methods, require_POST
 
 # Create your views here.
 
@@ -8,8 +10,13 @@ from django.views.decorators.http import require_http_methods
 def index(request):
     return render(request, 'account/layout.html')
 
+@require_POST
 def sign_in(request):
-    if request.method == 'GET':
-        return render(request, 'account/index.html')
+    '''Processes information sent by the user from the log-in page.
+    
+    This view accepts only 'POST' method requests.
+    '''
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('home:index'))
     
     
