e978e92f749d98706879182a8eacdad636ba0eb5
slicerrr0 <mrslicerr0@protonmail.com>
2023-05-17 17:18:43 -0400

    create sign in and registration form classes

--- account/forms.py
+++ account/forms.py
@@ -1,1 +1,8 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
+
+
+class SignInForm(AuthenticationForm):
+    pass
+
+class RegistrationForm(UserCreationForm):
+    pass
