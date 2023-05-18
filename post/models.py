2cf095f36803ff7fa050fb9710e41dbc47d78c0c
slicerrr0 <mrslicerr0@protonmail.com>
2023-05-17 21:01:05 -0400

    created `EmbeddedImage` model

--- post/models.py
+++ post/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import validate_image_file_extension
 from account.models import CustomUser
 from .choices import FlairColors
 from .validators import flair_validator
@@ -11,7 +12,12 @@ class Flair(models.Model):
     text = models.CharField(max_length=24, help_text='Flair message.')
 
 class Post(models.Model):
-    author  = models.ForeignKey(CustomUser, on_delete=models.PROTECT, help_text='User that made the post.')
+    author = models.ForeignKey(CustomUser, on_delete=models.PROTECT, help_text='User that made the post.')
     created_at = models.DateTimeField(help_text='Date and time the post was created.')
     flair = models.ForeignKey(Flair, blank=True, null=True, on_delete=models.PROTECT, help_text='Optional post flair.')
     message = models.TextField(max_length=1000, help_text='Post content.')
+
+class EmbeddedImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, help_text='Image embedded in post content.', validators=[validate_image_file_extension])
+    image = models.ImageField()
+    # https://docs.djangoproject.com/en/4.2/topics/http/file-uploads/
